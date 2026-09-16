afe47fb986df489ae3eaa1bb3ade2e4273cffd93
hhhhamidjavanshir13710205-ship-it <hhhhamidjavanshir13710205@gmail.com>
2026-09-16 18:13:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def main():
         print("STEP 2")
 
         response = requests.get(
-            "https://api.ourbit.com/api/v1/contract/ticker",
+            "https://futures.ourbit.com/api/v1/contract/ticker",
             timeout=20
         )
 
